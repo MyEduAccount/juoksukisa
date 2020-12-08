6cdade052f1fead9c533cbbd05437d5795e14100
MyEduAccount <69783983+MyEduAccount@users.noreply.github.com>
2020-12-08 15:16:57 +0200

    Add files via upload

--- Juha/dictionaries.py
+++ Juha/dictionaries.py
@@ -10,7 +10,7 @@ class Singleton(type):
         return cls._instances[cls]
     #
 #
-class _dictionaries_(metaclass=Singleton):
+class dictionaries(metaclass=Singleton):
     def __init__(self):
         self.dict_items
         self.dict_competitors
@@ -19,63 +19,75 @@ class _dictionaries_(metaclass=Singleton):
         self.dict_time
         self.dict_items2
            
-        #
-        def get_dict_items():
-            self.dict_items = dict_items = [
-                    
-                    {'first':'Mikko',   'last':'Meikäläinen','born':1940,'role':'guitar'},
-                    {'first':'Maija',   'last':'Meikäläinen','born':1942,'role':'bass'},
-                    {'first':'Mia',     'last':'Meikäläinen','born':1943,'role':'guitar'},
-                    {'first':'Matti',   'last':'Meikäläinen','born':1940,'role':'drums'},
-                    {'first':'Minna',   'last':'Meikäläinen','born':1926,'role':'producer'}
-                ]
+    #
+    def get_dict_items(self):
+        self.dict_items = [
+                
+                {'first':'Mikko',   'last':'Meikäläinen','born':1940,'role':'guitar'},
+                {'first':'Maija',   'last':'Meikäläinen','born':1942,'role':'bass'},
+                {'first':'Mia',     'last':'Meikäläinen','born':1943,'role':'guitar'},
+                {'first':'Matti',   'last':'Meikäläinen','born':1940,'role':'drums'},
+                {'first':'Minna',   'last':'Meikäläinen','born':1926,'role':'producer'}
+            ]
         
-        #id,name,kisa,matka,aika
-        def get_dict_competitors():
-                self.dict_competitors = dict_competitors = {
-            
-            "Mikko Meikäläinen"         : '0',
-            "Maija Meikäläinen"         : '1',
-            "Mia Meikäläinen"           : '2',
-            "Matti Meikäläinen"         : '3',
-            "Minna Meikäläinen"         : '4'  
-            
-        }
+        return self.dict_items
+    
+    #id,name,kisa,matka,aika
+    def get_dict_competitors(self):
+        self.dict_competitors =  {
         
-        def get_dict_competitors():
-            self.dict_competiton = dict_competiton = {
+                "Mikko Meikäläinen"         : '0',
+                "Maija Meikäläinen"         : '1',
+                "Mia Meikäläinen"           : '2',
+                "Matti Meikäläinen"         : '3',
+                "Minna Meikäläinen"         : '4'  
+                
+            }
             
-            "Meikäläisten jussinjuoksut"    : '2020-06-24',
-            "Meikäläisten kevätjuoksut"     : '2020-04-24'  
-            
-        }
-
-        def get_dict_distance():
-            self.dict_distance = dict_distance = {
-            
-            "Maastojuoksu"              : '3000m',
-            "Katujuoksu"                : '10000m',
-            "Ratajuoksu"                : '5000m'
-
-        }
-
-        def get_dict_time():
-            self.dict_time = dict_time = {
-            
-            ""         : '0',
-            "Maija Meikäläinen"         : '1',
-            "Mia Meikäläinen"           : '2',
-            "Matti Meikäläinen"         : '3',
-            "Minna Meikäläinen"         : '4'  
-            
-        }
+        return self.dict_competitors
+    
+    def get_dict_competitors(self):
+        self.dict_competiton = dict_competiton = {
         
-        def get_dict_items2():
-            self.dict_items2 = dict_items2 = [
-            
-            {'first':'John', 'last':'Lennon','born':1940,'role':'guitar'},
-            {'first':'Paul', 'last':'McCartney','born':1942,'role':'bass'},
-            {'first':'George','last':'Harrison','born':1943,'role':'guitar'},
-            {'first':'Ringo','last':'Starr','born':1940,'role':'drums'},
-            {'first':'George','last':'Martin','born':1926,'role':'producer'}
-        ]
+                "Meikäläisten jussinjuoksut"    : '2020-06-24',
+                "Meikäläisten kevätjuoksut"     : '2020-04-24'  
+                
+            }
+        
+        return self.dict_competiton
+
+    def get_dict_distance(self):
+        self.dict_distance = {
+        
+                "Maastojuoksu"              : '3000m',
+                "Katujuoksu"                : '10000m',
+                "Ratajuoksu"                : '5000m'
+
+            }
+        
+        return self.dict_distance
+
+    def get_dict_time(self):
+        self.dict_time = {
+        
+                ""         : '0',
+                "Maija Meikäläinen"         : '1',
+                "Mia Meikäläinen"           : '2',
+                "Matti Meikäläinen"         : '3',
+                "Minna Meikäläinen"         : '4'  
+                
+            }
+        
+        return self.dict_time  
+    
+    def get_dict_items2(self):
+        self.dict_items2 = [
+        
+                {'first':'John', 'last':'Lennon','born':1940,'role':'guitar'},
+                {'first':'Paul', 'last':'McCartney','born':1942,'role':'bass'},
+                {'first':'George','last':'Harrison','born':1943,'role':'guitar'},
+                {'first':'Ringo','last':'Starr','born':1940,'role':'drums'},
+                {'first':'George','last':'Martin','born':1926,'role':'producer'}
+            ]
+        
+        return self.dict_items2
